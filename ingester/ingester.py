6f07986e6a6e6392e68f8f6e31ed53793e5a4f36
Michael Zhao <mr_michaelzhao@hotmail.com>
2021-02-19 20:17:03 +0000

    Update ingester app

--- ingester/ingester.py
+++ ingester/ingester.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import functools
 
 from cryptofeed import FeedHandler
+from cryptofeed.backends.postgres import DeribitBookPostgres, DeribitTickerPostgres, DeribitTradePostgres
 from cryptofeed.callback import DeribitTickerCallback
 from cryptofeed.defines import BID, ASK, TRADES, L2_BOOK, PERPETURAL, OPTION, FUTURE, TICKER
 from cryptofeed.exchanges import Deribit
@@ -59,14 +60,24 @@ def get_time_from_timestamp(timestamp):
     return '%s.%03d' % (datetime.datetime.utcfromtimestamp(s).strftime('%H:%M:%S'), ms)
 
 subscriptions = {
-    PERPETURAL: [TICKER],
+    # PERPETURAL: [TICKER],
+    PERPETURAL: [],
     OPTION: [TICKER, TRADES, L2_BOOK],
-    FUTURE: [TICKER]
+    # FUTURE: [TICKER],
+    FUTURE: []
 }
+
+postgres_cfg = {'host': '127.0.0.1', 'user': 'postgres', 'db': 'fs', 'pw': None}
 
 def main():
     f = FeedHandler()
-    deribit = Deribit(max_depth = 1, subscription=get_new_subscription(), callbacks={L2_BOOK: book, TRADES: trade, TICKER: DeribitTickerCallback(callbacks={OPTION: ticker, PERPETURAL: perp_ticker})})
+    callbacks = {
+        TICKER: DeribitTickerPostgres(**postgres_cfg, cache_size=1000), 
+        TRADES: DeribitTradePostgres(**postgres_cfg),
+        L2_BOOK: DeribitBookPostgres(**postgres_cfg, cache_size=1000)
+    }
+    # deribit = Deribit(max_depth=1, subscription=get_new_subscription(), callbacks={L2_BOOK: book})#, TRADES: trade, TICKER: DeribitTickerCallback(callbacks={OPTION: ticker, PERPETURAL: perp_ticker})})
+    deribit = Deribit(max_depth=2, subscription=get_new_subscription(), callbacks=callbacks)
     f.add_feed(deribit)
     f.run(start_loop=True, tasks=[do_periodically_at(8, 1, 1, functools.partial(subscribe_to_new_subscription, deribit))])
     
